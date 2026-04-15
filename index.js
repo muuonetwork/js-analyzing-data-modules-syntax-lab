1f1420327a708d9ace9d140f731cf3ac511accfb
gideon musee <muuonetwork@gmail.com>
2026-04-15 15:18:52 +0300

    all test passed

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+// Step 5: Require datejs (ensure you've run 'npm install')
+require('datejs');
+
+/**
+ * Step 1: Create the combineUsers function 
+ * uses ...args to accept an indeterminate amount of arrays
+ */
+function combineUsers(...args) {
+    
+    // Step 2: Initialize the return object
+    let combinedObject = {
+        users: []
+    };
+
+    // Step 3: Loop through args to isolate each array
+    args.forEach(userArray => {
+        // Step 4: Use spread operator to merge arrays into the users attribute
+        combinedObject.users = [...combinedObject.users, ...userArray];
+    });
+
+    /**
+     * Step 5: Get today's date
+     * Note: datejs extends the native Date object. 
+     * We format it as M/d/yyyy (e.g., 4/15/2026)
+     */
+    combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+    // Step 7: Return object
+    return combinedObject;
+}
 
 
 
